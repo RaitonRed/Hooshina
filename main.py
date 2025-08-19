a5841a001dcf93bf55fa30f4de1fb867d13061df
Alireza <alirzahosinopoor.1390@gmail.com>
2025-08-19 14:38:02 +0330

    added start point

---
+++ main.py
@@ -0,0 +1,21 @@
+from src.dataset import Dataset
+from src.tokenizer import Tokenizer
+from src.model import Model
+from src.train import train
+from src.chat import chat
+import pandas as pd
+
+if __name__ == "__main__":
+    # آماده‌سازی دیتاست
+    df = pd.read_csv("data/telegram.csv")
+    tokenizer = Tokenizer()
+    tokenizer.build_vocab(df["message"].tolist() + df["reply_message"].tolist())
+
+    dataset = Dataset("data/proccessed/telegram.csv", tokenizer)
+    model = Model(vocab_size=len(tokenizer.word2id))
+
+    # آموزش
+    train(model, dataset, epochs=3)
+
+    # ورود به چت
+    chat(model, tokenizer)
